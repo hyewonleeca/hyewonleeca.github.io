c9817b4d3bde76257705ba2013e7d850e8591210
hyewon <everhyewonlee@github.com>
2023-07-20 16:55:56 -0400

    change the colour of texts

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ sections.forEach((section, index) => {
     const top = section.offsetTop;
     const height = section.offsetHeight;
 
-    if (window.pageYOffset >= top && window.pageYOffset < top + height) {
+    if (window.scrollY >= top && window.scrollY < top + height) {
     navLinks.forEach((navLink) => {
         navLink.classList.remove("active");
     });
